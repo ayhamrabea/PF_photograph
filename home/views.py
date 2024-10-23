239cf4753b84e247a738b4c89bb2ad4b5aba7f0e
ayhamrabea <ayhamrabea05@.com>
2024-10-23 18:49:07 +0300

    add static files

---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+
+
+def about(request):
+    return render(request,'about.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
